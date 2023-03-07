4522a4bf5ceea04a960cf6b965184d37d2e47751
Jeffrey Arbelaez <Jeffreyarbelaez@yahoo.com>
2023-03-07 16:31:44 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded",() => {
+    let changes = document.getElementById('text');
+    changes.textContent = 'This is really cool!'
+    console.log("changes")
+    });
